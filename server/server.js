4ffa92e97d37b1c2a42c568bc7caf2a8e6f65022
Bass Jansson <bassjansson@gmail.com>
2017-09-27 13:55:02 +0200

    TG-7 Sets server time-out to 1 minute and adds a custom style sheet.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
-var fs = require('fs');
 var PeerServer = require('peer').PeerServer;
+var fs = require('fs');
 
-var server = PeerServer({
-  port: 8888,
-  ssl: {
-    key: fs.readFileSync('../../ssl/keys/a4d39_e14ab_6d5722be2ebc50f81d68e7106eadb7ad.key'),
-    cert: fs.readFileSync('../../ssl/certs/vera_bassjansson_com_a4d39_e14ab_1513900799_446111a31b4d9d78162a058862d46cea.crt')
-  }
+var server = PeerServer(
+{
+    port: 8888,
+    timeout: 60000,
+    ssl:
+    {
+        key: fs.readFileSync('../../ssl/keys/a4d39_e14ab_6d5722be2ebc50f81d68e7106eadb7ad.key'),
+        cert: fs.readFileSync('../../ssl/certs/vera_bassjansson_com_a4d39_e14ab_1513900799_446111a31b4d9d78162a058862d46cea.crt')
+    }
 });
